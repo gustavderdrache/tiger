90cd75a966c48d73d6ef56dab9e4e96527efbd30
Alex Ford <gustavderdrache@gmail.com>
2019-06-11 22:34:31 -0400

    initial parser usage test

--- tigc/src/main.rs
+++ tigc/src/main.rs
@@ -1,21 +1,15 @@
 #![deny(missing_debug_implementations, future_incompatible)]
 #![warn(missing_copy_implementations)]
 
-mod expression;
-mod lexer;
-mod parser;
-mod pos;
-mod token;
+use tiger_syntax::Parser;
 
 fn main() {
-    env_logger::init();
-
     let inputs = &[
         "x := nil"
     ];
 
     for input in inputs {
-        let mut parser = parser::Parser::new(input);
+        let mut parser = Parser::new(input);
 
         println!("{}\n  {:#?}", input, parser.parse_program())
     }
